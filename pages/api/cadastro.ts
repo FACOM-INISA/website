cb6914c6bf3a0404b024ed4b19138975c6c1b71f
Leonardo Kazuyoshi <54998411+leonardo-kazu@users.noreply.github.com>
2022-11-11 16:12:41 -0400

    feat(cadastro.ts&prisma.schema): adiciona API de cadastro e modelo de usuario
    Adiciona API para cadastro de usuário e também cria o modelo do usuário no banco de dados
    
    re #35

---
+++ pages/api/cadastro.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+/*
+O script recebe um form que possui as seguintes características:
+  senha: string
+  nome : string
+  email: string
+Tenta adicionar o usuario para no banco de dados, se algum erro acontecer      |
+retorna 400 e o err, caso bem sucedido retorna '200: ok'.
+
+Para garantir que somente o hash da senha será guardado no BD a biblioteca
+bcrypt, com 10 rodadas de salt, devendo ser seguro o suficiente
+*/
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let body = req.body;
+  const prisma = new PrismaClient();
+  // Tenta inserir o usuário no banco de dados, caso resulte em algum erro,
+  // retorna o erro + status 400
+  try {
+    let hash: string = bcrypt.hashSync(body.senha, 10);
+    await prisma.usuario.create({
+      data: {
+        email: body.email,
+        nome: body.nome,
+        hash: hash
+      }
+    });
+    await prisma.$disconnect();
+    // res.redirect('/');
+    res.status(200).json({ 200: 'okay' })
+  } catch (err) {
+    await prisma.$disconnect();
+    res.status(400).json(err)
+  }
+}
