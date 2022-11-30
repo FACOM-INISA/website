bd8019f62b684a54ecedc5fae41efe89b13614d4
Leonardo Kazuyoshi <54998411+leonardo-kazu@users.noreply.github.com>
2022-11-30 14:02:55 -0400

    refactor(pages): corrije a implementação das imagens e fatoração do código

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import page from './signIn';
 
 const Home: NextPage = () => {
   return (
