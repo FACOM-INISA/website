94b32abc04f7114df4c20de7603d0b221cdf4527
Hudson S Borges <hudsonsilbor@gmail.com>
2023-04-05 17:07:14 -0400

    refactor: melhora apresentação da projeção no grafico de linhas

--- components/graficoLinha.tsx
+++ components/graficoLinha.tsx
@@ -14,12 +14,21 @@ import {
   Pie,
   Sector,
 } from 'recharts';
-import { Grid } from '@mui/material';
+import { capitalize, Grid } from '@mui/material';
 import { any } from 'bluebird';
 import Parto, { Predicao } from '../lib/Parto';
+import dayjs from 'dayjs';
+
+import 'dayjs/locale/pt-br';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+
+dayjs.locale('pt-br');
+dayjs.extend(customParseFormat);
+dayjs.extend(localizedFormat);
 
 type TipoDadosLocais = {
-  name: string;
+  date: Date;
   cesaria?: number;
   normais?: number;
   total?: number;
@@ -46,21 +55,21 @@ function OpenDataVisualization({
 
     let dataAux: Array<TipoDadosLocais> = registros?.map((reg) => {
       return {
-        name: `${reg.mes}/${2000 + reg.ano}`,
+        date: new Date(2000 + reg.ano, reg.mes, 1),
         cesaria: reg.parto_cesaria,
         normais: reg.parto_normais,
         total: reg.parto_total,
       };
     });
 
-    // const ultimo = dataAux[dataAux.length - 1];
-    // dataAux.push({ name: ultimo?.name, pred: ultimo?.normais }); // TODO - Alterar campo quando dados estiverem disponiveis
+    const ultimo = dataAux[dataAux.length - 1];
+    dataAux.push({ date: dayjs(ultimo?.date).add(1, 'days').toDate(), pred: ultimo?.normais }); // TODO - Alterar campo quando dados estiverem disponiveis
 
     predicoes
       .filter((pred) => pred.tipo_parto === 'normal') // TODO - Alterar campo quando dados estiverem disponiveis
       .forEach((predicao) => {
         dataAux.push({
-          name: `${predicao.mes}/${predicao.ano + 2000}`,
+          date: new Date(2000 + predicao.ano, predicao.mes, 1),
           pred: predicao.pred,
         });
       });
@@ -75,13 +84,25 @@ function OpenDataVisualization({
           <LineChart
             width={500}
             height={300}
-            data={data}
+            data={data?.map((data) => ({ ...data, xLabel: data.date?.getTime() }))}
             margin={{ top: 20, right: 50, left: 20, bottom: 5 }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
+            <XAxis
+              dataKey="xLabel"
+              type="number"
+              scale="time"
+              domain={[
+                data
+                  ?.filter((d) => d.pred)
+                  .at(-1)
+                  ?.date.getTime() || 'auto',
+                'auto',
+              ]}
+              tickFormatter={(value) => dayjs(value).format('MM/YYYY')}
+            />
             <YAxis />
-            <Tooltip />
+            <Tooltip labelFormatter={(value) => capitalize(dayjs(value).format('MMMM, YYYY'))} />
             <Legend />
 
             <Line
@@ -99,6 +120,15 @@ function OpenDataVisualization({
               name={'Predição de partos normais'}
               strokeWidth={2}
               dot={false}
+            />
+            <ReferenceLine
+              x={data
+                ?.filter((d) => d.pred)
+                .at(0)
+                ?.date.getTime()}
+              stroke="rgba(183,0,0,0.25)"
+              label="Projeção"
+              ifOverflow="visible"
             />
           </LineChart>
         </ResponsiveContainer>
