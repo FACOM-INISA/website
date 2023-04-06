13a8cef8ade7404259fb4544d77c998880fa6542
Hudson S Borges <hudsonsilbor@gmail.com>
2023-04-05 22:49:16 -0400

    refactor: configura tempo maximo do cookie para uma hora

--- lib/session.ts
+++ lib/session.ts
@@ -2,15 +2,15 @@
 Script para guardar a sessão do usuário
 */
 
+import dayjs from 'dayjs';
 import type { IronSessionOptions } from 'iron-session';
 import type { User } from '../pages/api/user/user';
 
 export const sessionOptions: IronSessionOptions = {
   password: process.env.SECRET_PASSWORD as string,
   cookieName: 'iron session',
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-  },
+  ttl: 60 * 60,
+  cookieOptions: { secure: process.env.NODE_ENV === 'production' },
 };
 
 declare module 'iron-session' {
